2a2331ed6c16c1bec118c1bcd424ecba3a4e561c
David Felipe Rendón Luna <biodfrl@gmail.com>
2024-03-01 22:18:47 +0000

    Correct second_file to second_fields

--- SCRIPTS/SUBSCRIPTS/gff_to_bed.R
+++ SCRIPTS/SUBSCRIPTS/gff_to_bed.R
@@ -49,6 +49,7 @@ name_specie <- substr(FILE, obj_grep + 1, obj_grep + attr(obj_grep, "match.lengt
 
 # Read file
 df <- read.delim(FILE, header = FALSE, col.names = c("seqname", "source", "feature", "start", "end", "score", "strand", "frame", "atribute"))
+gff <- rtracklayer::import(FILE, "gff")
 
 # Split df attributes by semicolons
 splited <- strsplit(df$atribute, ";")
@@ -83,9 +84,21 @@ bed_df <- bed_df[grepl("RSS|_exon_", bed_df$name),]
 # Eliminate "ID=" from name
 bed_df$name <- gsub("ID=", "", bed_df$name)
 
+# Make second field
+second_field <- c()
+c <- 1
+for (i in seq_along(bed_df$name)) {
+  if (grepl("RSS", bed_df$name[c])) {
+    second_field[c] <- "IGHV_RSS3"  
+  } else if (grepl("IGHV_exon", bed_df$name[c])) {
+    second_field[c] <- "IGHV_exon"  
+  } 
+  c <- c + 1
+}
+
 # Make final attribute for bed column "name"
 bed_df$name <- paste0(name_specie, "_", gsub("_plus|_minus", "", bed_df$name), "|", 
-                      "|", 
+                      second_field, "|", 
                       "|",
                       with(bed_df,ifelse(strand == "+", "FOR", ifelse(strand == "-", "REV", "." ))))
 
@@ -102,6 +115,49 @@ df_minus$thickStart <- df_minus$thickStart - 1
 
 # Make bed again
 bed_df <- rbind(df_plus, df_minus)
+
+# Separate genes 
+genes <- gff[grepl("IGHV_gene_.*_", gff$ID)]
+features <- gff[grepl("RSS_V-associated_.*|IGHV_exon_.*_", gff$ID)]
+
+# Extend genes ranges
+genes@ranges@start <- as.integer(genes@ranges@start - 100)
+genes@ranges@width <- as.integer(genes@ranges@width + 200)
+
+#Make overlaps
+gr <- GenomicRanges::findOverlaps(genes, features)
+
+# Return ranges to original
+genes@ranges@start <- as.integer(genes@ranges@start + 100)
+genes@ranges@width <- as.integer(genes@ranges@width - 200)
+
+vec_conversion <- c()
+for (i in seq_along(gr)) {
+  vec_conversion[gr@to[i]] <- gr@from[i]
+}
+
+# Make final attribute for bed column "name"
+bed_df$name <- paste0(paste0(name_specie, "_IGHV_", sprintf('%0.3d', vec_conversion)), "|", 
+                      second_field, "|", 
+                      "|",
+                      with(bed_df,ifelse(strand == "+", "FOR", ifelse(strand == "-", "REV", "." ))), "|")
+
+
+bed_df$seg_size <- abs(bed_df$chromStart - bed_df$chromEnd) 
+
+for (i in 1:nrow(bed_df)) {
+  if (bed_df$seg_size[i] < 60 & grepl("IGHV_exon", bed_df$name[i])) {
+    second_field[i] <- "SP_exon"
+  }
+}
+
+# Make final attribute for bed column "name"
+bed_df$name <- paste0(paste0(name_specie, "_IGHV_", sprintf('%0.3d', vec_conversion)), "|", 
+                      second_field, "|", 
+                      "|",
+                      with(bed_df,ifelse(strand == "+", "FOR", ifelse(strand == "-", "REV", "." ))), "|")
+
+bed_df$seg_size <- NULL
 
 # Save results
 write.table(bed_df, file = OUTFILE, quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
